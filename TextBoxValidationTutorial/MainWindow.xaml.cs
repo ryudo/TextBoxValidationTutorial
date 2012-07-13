1c86e27fed252d8f50e58bd4d0c9a22868e201dc
Ryudo NAKAGAWA <nkgw.ry@gmail.com>
2012-07-13 13:00:00 +0900

    init commit

---
+++ TextBoxValidationTutorial/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace TextBoxValidationTutorial
+{
+	using System.Windows;
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		/// <summary>
+		/// 依存関係プロパティ バインディング機能を使うために必要
+		/// </summary>
+		public static readonly DependencyProperty StarNameProperty = DependencyProperty.Register(
+			"StarName", typeof(string), typeof(MainWindow), new PropertyMetadata(default(string)));
+
+		/// <summary>
+		/// テキストボックスとバインディングするためのプロパティ
+		/// </summary>
+		public string StarName
+		{
+			get
+			{
+				return (string)this.GetValue(MainWindow.StarNameProperty);
+			}
+
+			set
+			{
+				this.SetValue(MainWindow.StarNameProperty, value);
+			}
+		}
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// プレゼン用 テキストボックスのバリデーションエラーを捕まえることができる
+		/// </summary>
+		/// <param name="sender">送信オブジェクト</param>
+		/// <param name="e">バリデーションエラーに関するパラメータ</param>
+		private void OnValidationError(object sender, ValidationErrorEventArgs e)
+		{
+			if (e.Action == ValidationErrorEventAction.Added)
+			{
+				// バリデーションエラーが発生したときの処理を追加できます
+			}
+		}
+	}
+}
---
+++ TextBoxValidationTutorial/MyValidationSample.cs
@@ -0,0 +1,33 @@
+﻿namespace TextBoxValidationTutorial
+{
+	using System;
+	using System.Globalization;
+	using System.Windows.Controls;
+
+	/// <summary>
+	/// バリデーションルールのクラス
+	/// </summary>
+	public class MyValidationSample : ValidationRule
+	{
+		/// <summary>
+		/// バリデーションを行うメソッド
+		/// </summary>
+		/// <param name="value">入力値</param>
+		/// <param name="cultureInfo">カルチャ</param>
+		/// <returns>バリデーション結果を示す戻り値</returns>
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			double number = 0;
+			try
+			{
+				number = Convert.ToDouble(value.ToString());
+			}
+			catch (Exception)
+			{
+				return new ValidationResult(false, "値が数値じゃありません");
+			}
+
+			return number == 0.0 ? new ValidationResult(false, "値が数値じゃありません") : new ValidationResult(true, null);
+		}
+	}
+}
